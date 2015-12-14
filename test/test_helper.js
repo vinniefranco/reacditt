0dec95f85dfb189674ee0485a58cb115b29a3b8e
Vincent Franco <vince@freshivore.net>
2015-12-13 22:52:55 -0800

    Removing eslint, moving to standardjs, fixing some formatting errors

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,15 +1,15 @@
-import chai from "chai"
-import chaiImmutable from "chai-immutable"
-import jsdom from "jsdom"
+import chai from 'chai'
+import chaiImmutable from 'chai-immutable'
+import jsdom from 'jsdom'
 
-const doc = jsdom.jsdom("<!doctype html><html><body></body></html>")
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
 const win = doc.defaultView
 
 global.document = doc
 global.window = win
 
-Object.keys(window).forEach((key) => {
-  if(!(key in global)) {
+Object.keys(window).forEach(key => {
+  if (!(key in global)) {
     global[key] = window[key]
   }
 })
